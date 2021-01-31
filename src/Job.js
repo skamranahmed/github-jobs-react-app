578f8acdbcc8a69bb5398be458fd557060cef5e8
skahmed <skahmed@myamu.ac.in>
2021-01-31 20:58:54 +0530

    Updated the Job component to add bootstrap styles

--- src/Job.js
+++ src/Job.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Card, Badge } from "react-bootstrap";
 
-const Job = ({job}) => {
-    return (
-        <div>
-            {job.title}
+const Job = ({ job }) => {
+  return (
+    <Card>
+      <Card.Body>
+        <div className="d-flex justify-content-between">
+          <div>
+            <Card.Title>
+              {job.title} -{" "}
+              <span className="text-muted font-weight-light">
+                {job.company}
+              </span>
+            </Card.Title>
+            <Card.Subtitle className="text-muted mb-2">
+              {new Date(job.created_at).toLocaleDateString()}
+            </Card.Subtitle>
+            <Badge variant="secondary" className="mr-2">
+              {job.type}
+            </Badge>
+            <Badge variant="secondary">{job.location}</Badge>
+          </div>
         </div>
-    )
-}
+      </Card.Body>
+    </Card>
+  );
+};
 
-export default Job
+export default Job;
