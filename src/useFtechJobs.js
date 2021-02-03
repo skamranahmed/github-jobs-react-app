de887ebaf41cba6fb846ebc6acf805ee177b12f7
skahmed <skahmed@myamu.ac.in>
2021-02-04 02:47:34 +0530

    Added heroku cors as proxy

--- src/useFtechJobs.js
+++ src/useFtechJobs.js
@@ -1,9 +1,9 @@
 import { useReducer, useEffect } from "react";
 import axios from "axios";
 
-// const BASE_URL =
-//   "https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json";
-const BASE_URL = "/positions.json";
+const BASE_URL =
+  "https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json";
+// const BASE_URL = "/positions.json";
 
 const ACTIONS = {
   MAKE_REQUEST: "make-request",
